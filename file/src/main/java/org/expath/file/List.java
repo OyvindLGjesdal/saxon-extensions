1c0b6d292657e4d98f8ef331416fef4583afe875
Øyvind gjesdal <oyvind@localhost.localdomain>
2021-02-12 21:24:05 +0100

    test actions

--- file/src/main/java/org/expath/file/List.java
+++ file/src/main/java/org/expath/file/List.java
@@ -19,15 +19,12 @@ package org.expath.file;
 
 //import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.PathMatcher;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import org.expath.file.error.FileException;
 
@@ -96,29 +93,44 @@ public class List extends FileFunctionDefinition {
     private final boolean recursive;
     private final Path root;
 
+
     public Finder(Path root, boolean recursive, String pattern) {
       this.root = root;
       this.recursive = recursive;
       if (pattern == null) {
         this.matcher = null;
       } else {
-        this.matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
+        String matcherString;
+        if (pattern.startsWith("regex:")) {
+          matcherString = pattern;
+        }
+        else {
+        matcherString = "glob:" + pattern;
+        }
+        this.matcher = FileSystems.getDefault().getPathMatcher(matcherString);
       }
     }
 
     private void match(Path file) {
-      Path name = file.getFileName();
-      if (name != null && (matcher == null || matcher.matches(name))) {
-        if (file.isAbsolute()) {
-          file = root.relativize(file);
-        }
-        stringValues.add(StringValue.makeStringValue(file.toString()));
+      //Path name = file.getFileName();
+      if ((matcher == null || matcher.matches(file.getFileName()))) {
+    //    if (file.isAbsolute()) {
+        stringValues.add(StringValue.makeStringValue(root.relativize(file).toString()));
+
+      }
+    }
+
+    private void match(Path file, Path filename) {
+      //Path name = file.getFileName();
+      if ((matcher == null || matcher.matches(file.getFileName()))) {
+        //    if (file.isAbsolute()) {
+        stringValues.add(StringValue.makeStringValue(root.relativize(file).toString()));
       }
     }
         
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-      match(file);      
+      match(file);
       return FileVisitResult.CONTINUE;
     }
     
@@ -126,16 +138,18 @@ public class List extends FileFunctionDefinition {
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
       if (dir.equals(root)) {
         return FileVisitResult.CONTINUE;
-      }      
+      }
       match(dir);
-      if (recursive) {
+      if (recursive && (matcher == null || matcher.matches(dir.getFileName()))) {
         return FileVisitResult.CONTINUE;
-      } 
-      return FileVisitResult.SKIP_SUBTREE;            
+      }
+      else {
+        return FileVisitResult.SKIP_SUBTREE;
+      }
     }
 
     @Override
-    public FileVisitResult visitFileFailed(Path file, IOException exc) { 
+    public FileVisitResult visitFileFailed(Path file, IOException exc) {
       return FileVisitResult.CONTINUE;
     }
     
@@ -159,7 +173,7 @@ public class List extends FileFunctionDefinition {
           throw new FileException(String.format("Path \"%s\" does not point to an existing directory",
                   file.toAbsolutePath()), FileException.ERROR_PATH_NOT_DIRECTORY);
         }
-        //Path rootPath = path.toAbsolutePath();
+        file = file.toAbsolutePath();
         boolean recursive = false;
         if (arguments.length > 1) {
           recursive = ((BooleanValue) arguments[1].head()).getBooleanValue();
@@ -170,6 +184,7 @@ public class List extends FileFunctionDefinition {
         }
         Finder finder = new Finder(file, recursive, pattern);
         Files.walkFileTree(file, finder);
+
         ArrayList<StringValue> result = finder.getStringValues();
         return new ZeroOrMore<>(finder.getStringValues().toArray(new StringValue[result.size()]));
       } catch (FileException fe) {
